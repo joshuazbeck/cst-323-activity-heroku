318ac52dc3a8d59615516408de69fafafa87f0bc
Joshua Beck <josh.beck@switchbackcms.com>
2023-01-21 14:04:52 -0600

    adding logging

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -15,18 +15,22 @@ class CarController
      */
     public function edit($car)
     {
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD ENTER CarController.edit($car)');
         $car = \App\Models\Car::where('id', '=', $car)->first();
         if ($car == null) {
             abort(404, "No car found with that id");
         }
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD EXIT CarController.edit($car)');
         return view('edit-car', ["car"=>$car]);
     }
 
     public function update($id){
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD ENTER CarController.update($id)');
         $params = request()->all();
         unset($params["_token"]);
         \App\Models\Car::where('id', '=', $id)->update($params);
         $cars = \App\Models\Car::all();
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD EXIT CarController.update($id)');
         return view('list-car', ["cars" => $cars]);
     }
     /**
@@ -37,13 +41,17 @@ class CarController
      */
     public function add()
     {
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD ENTER CarController.add()');
         $car = new \App\Models\Car();
+
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD EXIT CarController.add()');
 
         return view('add-car', ["car" => $car]);
     }
 
     public function store()
     {
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD ENTER CarController.store()');
         $car = new \App\Models\Car();
 
         $car->carname = request('carname');
@@ -54,6 +62,9 @@ class CarController
         $car->description = request('description');
         $car->brandname = request('brandname');
         $car->save();
+
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD EXIT CarController.store()');
+
         return $this->list();
     }
 
@@ -65,30 +76,35 @@ class CarController
      */
     public function view($id)
     {
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD ENTER CarController.view($id)');
+
         $car = \App\Models\Car::where('id', '=', $id)->first();
 
         if ($car == null){
             abort(404, "No car found with that id");
         }
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD EXIT CarController.view($id)');
         return view('display-car', ["car"=>$car]);
     }
 
     public function delete($car)
     {
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD ENTER CarController.delete($car)');
+
         \App\Models\Car::where('id', '=', $car)->delete();
         $cars = \App\Models\Car::all();
+
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD EXIT CarController.delete($car)');
         return view('list-car', ["cars" => $cars]);
     }
     public function list()
     {
-//        $users = DB::table('car_info')->get();
-//
-//        $name = "";
-//        foreach ($users as $user)
-//        {
-//            $name = $user->username;
-//        }
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD ENTER CarController.list()');
+
         $cars = \App\Models\Car::all();
+
+        \Illuminate\Support\Facades\Log::channel('single')->info(date('[Y-m-d H:i:s] ') . ': ' . ' METHOD EXIT CarController.list()');
+
         return view('list-car', ["cars" => $cars]);
     }
 }
